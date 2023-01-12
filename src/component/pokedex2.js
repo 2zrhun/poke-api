9d601f2f0b5ff3fa7e261b297abaa20a76d9bbaf
minaarafat19 <aminayoussoufarafat@gmail.com>
2023-01-12 14:02:22 +0100

    ww

--- src/component/pokedex2.js
+++ src/component/pokedex2.js
@@ -12,11 +12,14 @@ export default function Pokedex2(props) {
   const [Attack, setAttack] = useState("");
   const [order, setorder] = useState("");
   const [defense, setDefense] = useState("");
-  const [chosedPoke, setChosedPoke] = useState("");
+  const [name, setName] = useState("");
+  const [Id, setId] = useState("");
+  const [chosedPoke, setChosedPoke] = useState([]);
+  const [add, setAdd] = useState("");
   const navigate = useNavigate();
   const Mytoken = localStorage.getItem("Saved_Token");
 
-  let url = "https://pokeapi.co/api/v2/pokemon/";
+  /* let url = "https://pokeapi.co/api/v2/pokemon/";
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
@@ -25,10 +28,11 @@ export default function Pokedex2(props) {
         setRoundpoke(data.results);
       })
       .catch((err) => console.error(err));
-  }, [url]);
+  }, [url]);*/
 
   const getPokemon = (event) => {
     ChangePiece();
+
     let nb_piece_btn = parseInt(event.currentTarget.id);
     let calcul_piece = piece - nb_piece_btn;
     setPiece(calcul_piece);
@@ -40,7 +44,7 @@ export default function Pokedex2(props) {
       fetch(`https://pokeapi.co/api/v2/pokemon/${random_pokeid}/`)
         .then((resultat) => resultat.json())
         .then((data) => {
-          setRoundpoke(data);
+          setRoundpoke((current) => [...current, data]);
           let i = 0;
           roundPoke.new_types = [];
           let new_id = [];
@@ -49,9 +53,12 @@ export default function Pokedex2(props) {
 
           console.log("HP ", data.stats[0].base_stat);
           console.log("Attack ", data.stats[1].base_stat);
+
           setHp(data.stats[0].base_stat);
           setAttack(data.stats[1].base_stat);
           setorder(data.order);
+          setName(data.name);
+          setId(data.id);
           setDefense(data.stats[2].base_stat);
           for (i === 0; i < data.types.length; i++) {
             res = data.types[i].type.name;
@@ -60,12 +67,13 @@ export default function Pokedex2(props) {
 
             setType(data.types[i].type.name);
           }
+
           new_id.push(data.id);
           setImg(data.sprites.front_default);
+          //console.log("ooo", roundPoke);
         })
         .catch((err) => console.error(err));
-      let results = [];
-      setChosedPoke(roundPoke);
+
       const savedPokemons = JSON.stringify(roundPoke);
       localStorage.setItem("savedPoke", ...savedPokemons);
     } else {
@@ -74,6 +82,7 @@ export default function Pokedex2(props) {
       );
     }
   };
+
   async function ChangePiece() {
     const response3 = await fetch(
       `http://localhost:5000/updatepiece/${localStorage.getItem(
@@ -91,7 +100,7 @@ export default function Pokedex2(props) {
   }
   const Test = async (id) => {
     var elt = this;
-    console.log("test", Type);
+    //console.log("test", Type);
     await fetch(
       `http://localhost:5000/record/add/${localStorage.getItem(
         "Saved_UserId"
@@ -104,8 +113,8 @@ export default function Pokedex2(props) {
         body: JSON.stringify({
           //new_id: [roundPoke[id - 1].id],
           userId: localStorage.getItem("Saved_UserId"),
-          new_id: roundPoke.id,
-          name: roundPoke.name,
+          new_id: Id,
+          name: name,
           new_types: Type,
           new_image: Img,
           hp: Hp,
@@ -119,6 +128,14 @@ export default function Pokedex2(props) {
       return;
     });
     alert("ajouter le prochain pokemon");
+  };
+  function tt() {
+    let tmp = [...roundPoke];
+    tmp.push(roundPoke);
+    setChosedPoke(tmp);
+  }
+  const gt = () => {
+    tt();
   };
   if (Mytoken == null) {
     //alert("Veuillez vous connecter ! ");
@@ -138,6 +155,38 @@ export default function Pokedex2(props) {
             <div className="row">
               <div className="column">
                 <div className="card">
+                  {roundPoke.map((ad, index) => (
+                    <tr key={"index-" + index}>
+                      <br></br>
+                      Attack:<b>{ad.stats[1].base_stat}</b>
+                      Hp: <b>{ad.stats[0].base_stat}</b>
+                      Order: <b>{ad.order}</b>
+                      Defense: <b>{ad.stats[2].base_stat}</b>
+                      <br></br>
+                      ID: <td>{ad.id}</td>
+                      NAME<td>{ad.name}</td>
+                      type:<td>{ad.types[0].type.name}</td>
+                      <td>
+                        <img src={ad.sprites.front_default} />
+                      </td>
+                      <button id={ad.id} onClick={() => Test(ad.id)}>
+                        Add To pokedex
+                      </button>
+                    </tr>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+        {console.log("the adversaire", roundPoke)}
+      </div>
+    );
+  }
+}
+/**<div className="row">
+              <div className="column">
+                <div className="card">
                   id#<b> {roundPoke.id}</b>
                   <img src={Img} alt="d" />
                   <img className="back" src="../image/download.png" />
@@ -152,16 +201,28 @@ export default function Pokedex2(props) {
                   Defense: <b>{defense}</b>
                   <br></br>
                   <p>{roundPoke.tableauAdded}</p>
-                  {console.log(roundPoke)}
                   <button id={roundPoke.id} onClick={() => Test(roundPoke.id)}>
                     Add To pokedex
                   </button>
                 </div>
               </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+            </div> */
+/**<div>
+              {roundPoke.map((ad, index) => (
+                <tr key={"index-" + index}>
+                  <br></br>
+                  Type: <b>{Type}</b>
+                  Attack:<b>{Attack}</b>
+                  Hp: <b>{Hp}</b>
+                  Order: <b>{order}</b>
+                  Defense: <b>{defense}</b>
+                  <br></br>
+                  ID: <td>{ad.id}</td>
+                  NAME<td>{ad.name}</td>
+                  type:<td>{ad.Type}</td>
+                  <td>
+                    IMAGE: <img src={ad.new_image} />
+                  </td>
+                </tr>
+              ))}
+            </div> */
